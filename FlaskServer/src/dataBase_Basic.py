5ef2d1c3abc04c7259f2eb9556d423b7a5faa804
bittle95 <bittle95@naver.com>
2022-08-23 20:18:01 +0900

    서버 테스트 완료(방화벽 이슈)

--- FlaskServer/src/dataBase_Basic.py
+++
@@ -1,8 +0,0 @@
-
-class DataBase:
-    def __init__(self):
-        self.userID = ""
-        self.userPW = ""
-
-    def ToString(self):
-        return self.userID + "@" + self.userPW
---
+++ FlaskServer/src/testData.py
@@ -0,0 +1,9 @@
+
+class NRCData:
+    def __init__(self, id, stride, width, area, size, content):
+        self.id = "2"
+        self.stride = 0
+        self.width = 0
+        self.area = 0
+        self.size = 0
+        self.content = 0
